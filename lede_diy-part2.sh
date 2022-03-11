c18c575ab378accf955d7b98a1f13214acdf8bfb
尘事尘飞 <86181542+ficheny@users.noreply.github.com>
2022-03-11 20:59:20 +0800

    Create lede_diy-part2.sh

---
+++ lede_diy-part2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#1. 修改默认IP
+sed -i 's/192.168.1.1/192.168.124.1/g' package/base-files/files/bin/config_generate
+
+#2. web登陆密码从password修改为空
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3.固件版本号添加个人标识和日期
+sed -i "s/Openwrt /FICHEN $(TZ=UTC-8 date "+%Y-%m-%d")@OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+#4.编译的固件文件名添加日期
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(TZ=UTC-8 date +%Y%m%d-%H%M)-/g' include/image.mk
