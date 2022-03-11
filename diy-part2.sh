0e1b458b6faddc7d8828b33a669b82969a71bf7a
尘事尘飞 <86181542+ficheny@users.noreply.github.com>
2022-03-11 16:04:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,15 +14,15 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #1. 修改默认IP
-sed -i 's/10.10.10.1/192.168.124.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.124.1/g' package/base-files/files/bin/config_generate
 
 #2. web登陆密码从boos修改为password)
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 #sed -i '/mirrors.cloud.tencent.com/a sed -i \'s\/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::\/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::\/g\' \/etc\/shadow' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #3.固件版本号添加个人标识和日期
-sed -i "s/Openwrt /FICHEN $(TZ=UTC-8 date "+%Y-%m-%d")@OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "s/Openwrt /FICHEN $(TZ=UTC-8 date "+%Y-%m-%d")@OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #4.编译的固件文件名添加日期
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M)-/g' openwrt/include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M)-/g' include/image.mk
