582f0d4968bf7d40be2ec7a6d7a5d1b242bb18c1
尘事尘飞 <86181542+ficheny@users.noreply.github.com>
2022-03-12 12:35:08 +0800

    Update Boos4721_diy-part2.sh

--- Boos4721_diy-part2.sh
+++ Boos4721_diy-part2.sh
@@ -22,7 +22,7 @@ sed -i 's/10.10.10.1/192.168.123.1/g' package/base-files/files/bin/config_genera
 sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #3.固件版本号添加个人标识和日期
-sed -i "s/Openwrt /FICHEN($(TZ=UTC-8 date "+%Y-%m-%d"))@OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/Openwrt /FICHEN($(date "+%Y-%m-%d"))@OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #4.编译的固件文件名添加日期
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(date "+%Y%m%d-%H%M")-/g' include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date "+%Y%m%d-%H%M")-/g' include/image.mk
