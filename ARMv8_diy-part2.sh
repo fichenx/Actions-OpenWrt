a4402077cc7d8a040607f01d807edb4e9aaa6ff2
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2023-06-06 17:38:16 +0800

    Update ARMv8_diy-part2.sh

--- ARMv8_diy-part2.sh
+++ ARMv8_diy-part2.sh
@@ -65,8 +65,8 @@ sed -i 's/\[services\]/\[vpn\]/g'  feeds/luci/applications/luci-app-frps/luasrc/
 
 #nps（修改nps源为yisier）
 sed -i 's/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/codeload.github.com\/yisier\/nps\/tar.gz\/v$(PKG_VERSION)?/g' feeds/packages/net/nps/Makefile
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=0.26.16/g' feeds/packages/net/nps/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=8e1405cc69b3d509d9397c2423b6b6a7e3ee8a6b69ec3481e15f9846ab0c9852/g' feeds/packages/net/nps/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=0.26.16.1/g' feeds/packages/net/nps/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=2fb8a19d2bd34d6a009f14d1c797169f09801eb814f57ebf10156ffdb78f2457/g' feeds/packages/net/nps/Makefile
 #luci-app-nps（修改nps显示位置）
 sed -i 's/"services"/"vpn"/g'  feeds/luci/applications/luci-app-nps/luasrc/controller/nps.lua
 sed -i 's/\[services\]/\[vpn\]/g'  feeds/luci/applications/luci-app-nps/luasrc/view/nps/nps_status.htm
