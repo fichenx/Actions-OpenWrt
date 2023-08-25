9213577306de151a5dd049a42d9230512ba1ce09
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2023-08-25 17:43:22 +0800

    Update Newifi-D2_diy-part2.sh

--- sh/Newifi-D2_diy-part2.sh
+++ sh/Newifi-D2_diy-part2.sh
@@ -33,7 +33,7 @@ rm -rf feeds/luci/applications/luci-app-design-config && git clone --depth 1 htt
 #6.添加自动挂载磁盘脚本
 #mkdir -p files/etc/hotplug.d/block && wget -O files/etc/hotplug.d/block/30-usbmount https://raw.githubusercontent.com/fichenx/P3TERX_Actions-OpenWrt/main/files/etc/hotplug.d/block/30-usbmount && chmod 755 files/etc/hotplug.d/block/30-usbmount
 mkdir -p files/etc/hotplug.d/block/
-cp -rf $GITHUB_WORKSPACE/general/newifi3/files/etc/hotplug.d/block/30-usbmount files/etc/hotplug.d/block/
+cp -rf $GITHUB_WORKSPACE/backup/newifi3/files/etc/hotplug.d/block/30-usbmount files/etc/hotplug.d/block/
 chmod 755 files/etc/hotplug.d/block/30-usbmount
 
 #7.修改主机名
