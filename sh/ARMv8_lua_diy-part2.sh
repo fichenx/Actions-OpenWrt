af1690b354dd33a4c117b226f30d8457b119de6c
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2024-11-07 06:47:46 +0800

    Update ARMv8_lua_diy-part2.sh

--- sh/ARMv8_lua_diy-part2.sh
+++ sh/ARMv8_lua_diy-part2.sh
@@ -105,14 +105,14 @@ sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-
 ##########添加&修改插件#########
 
 ##使用openwrt官方版elfutils
-rm -rf package/libs/elfutils
-git_svn main https://github.com/openwrt/openwrt package/libs/elfutils
+#rm -rf package/libs/elfutils
+#git_svn main https://github.com/openwrt/openwrt package/libs/elfutils
 
 
 ##修复elfutils编译错误
 #1、修复lede版elfutils0.188版编译错误
 sed -i "s|TARGET_CFLAGS += -D_GNU_SOURCE -Wno-unused-result -Wno-format-nonliteral|TARGET_CFLAGS += -D_GNU_SOURCE -Wno-unused-result -Wno-format-nonliteral -Wno-error=use-after-free|g" package/libs/elfutils/Makefile
-#1、修复openwrt官方版elfutils0.191版elfutils编译错误
+#2、修复openwrt官方版elfutils0.191版elfutils编译错误
 sed -i "s|CONFIG_GCC_USE_VERSION_11|CONFIG_GCC_USE_VERSION_12|g" package/custom2/elfutils/Makefile
 
 
