a853d048a026bfa4c24d40b27cdab20c59ca7c67
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2025-06-24 23:44:17 +0800

    Create ax6_imm_diy-part2.sh

---
+++ sh/ax6_imm_diy-part2.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+function git_sparse_clone() {
+  branch="$1" rurl="$2" && shift 2
+  rootdir="$PWD"
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl temp_sparse
+  #git clone -b $branch --single-branch --no-tags --depth 1 --filter=blob:none --no-checkout $rurl temp_sparse
+  cd temp_sparse
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  pkg=`echo $@ | tr ' ' '\n' | rev | cut -d'/' -f 1 | rev | tr '\n' ' ' `
+  #git checkout $branch -- $@
+  [ -d ../package/custom ] && cd ../package/custom && rm -rf $pkg && cd "$rootdir"/temp_sparse
+  mv -n $@ ../
+  cd ..
+  rm -rf temp_sparse
+  }
+  
+function git_svn() {
+  #branch="$1" rurl="$2" localdir="$3" && shift 3
+  branch="$1" rurl="$2" && shift 2
+  rootdir="$PWD"
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl temp_svn
+  #git clone -b $branch --single-branch --no-tags --depth 1 --filter=blob:none --no-checkout $rurl temp_svn
+  cd temp_svn
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  pkg=`echo $@ | tr ' ' '\n' | rev | cut -d'/' -f 1 | rev | tr '\n' ' ' `
+  #git checkout $branch -- $@
+  [ -d ../package/custom ] && cd ../package/custom && rm -rf $pkg && cd "$rootdir"/temp_svn
+  mv -n $@ ../package/custom2/
+  cd ..
+  rm -rf temp_svn
+  }
+  
+function merge_package(){
+    branch=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    repo=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $3 | rev | cut -d'/' -f 1 | rev`
+	rootdir="$PWD"
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone -b $1 --depth=1 --single-branch $2
+  [ -d package/custom ] && cd package/custom && rm -rf $3 && cd "$rootdir"
+    mv $3 package/custom2/
+    rm -rf $repo
+}
+rm -rf package/custom2; mkdir package/custom2
+
+##########固件配置修改#########
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#修改默认IP
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# web登陆密码从password修改为空
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#固件版本号添加个人标识和日期
+sed -i "/DISTRIB_DESCRIPTION='*'/d" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='FICHEN($(TZ=UTC-8 date +%Y.%m.%d))@immortalwrt '" >> package/base-files/files/etc/openwrt_release
+
+#修改主机名
+sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+
+
