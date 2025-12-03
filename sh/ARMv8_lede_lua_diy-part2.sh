37625fcfe4748d02eb732c044921468c601fd60c
fichenx@gmail.com <fichenx@gmail.com>
2025-12-03 22:50:50 +0800

    升级docker版本到最新版

--- sh/ARMv8_lede_lua_diy-part2.sh
+++ sh/ARMv8_lede_lua_diy-part2.sh
@@ -243,7 +243,22 @@ sed -i 's/\[services\]/\[vpn\]/g'  feeds/luci/applications/luci-app-frps/luasrc/
 sed -i 's/"services"/"vpn"/g'  feeds/luci/applications/luci-app-nps/luasrc/controller/nps.lua
 sed -i 's/\[services\]/\[vpn\]/g'  feeds/luci/applications/luci-app-nps/luasrc/view/nps/nps_status.htm
 
-
+#删除自带和breakingbadboy自定义版本的dockerd、docker及依赖containerd、runc，使用自定义docker版本
+#rm -rf feeds/fichenx/dockerd feeds/fichenx/docker
+rm -rf feeds/packages/utils/dockerd
+git_sparse_clone main https://github.com/fichenx/openwrt-package dockerd && mv -n dockerd feeds/packages/utils/dockerd
+#git_sparse_clone master https://github.com/coolsnowwolf/packages utils/dockerd && mv -n dockerd feeds/packages/utils/dockerd
+rm -rf feeds/packages/utils/docker
+git_sparse_clone main https://github.com/fichenx/openwrt-package docker && mv -n docker feeds/packages/utils/docker
+#git_sparse_clone master https://github.com/coolsnowwolf/packages utils/docker && mv -n docker feeds/packages/utils/docker
+#rm -rf feeds/packages/utils/containerd
+#git_sparse_clone master https://github.com/coolsnowwolf/packages utils/containerd && mv -n containerd feeds/packages/utils/containerd
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.2.0/g' feeds/packages/utils/containerd/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca/g' feeds/packages/utils/containerd/Makefile
+#rm -rf feeds/packages/utils/runc
+#git_sparse_clone master https://github.com/coolsnowwolf/packages utils/runc && mv -n runc feeds/packages/utils/runc
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.4.0/g' feeds/packages/utils/runc/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=94d566d8b017d6cdffc684560a4f069bb87f86534976c41d768711c85e194884/g' feeds/packages/utils/runc/Makefile
 
 #########修复编译错误#########
 
@@ -315,23 +330,6 @@ sed -i '/CONFIG_PACKAGE_luci-app-rtp2httpd=y/d' .config
 sed -i '/CONFIG_PACKAGE_luci-i18n-rtp2httpd-en=y/d' .config
 sed -i '/CONFIG_PACKAGE_luci-i18n-rtp2httpd-zh-cn=y/d' .config
 sed -i '/CONFIG_PACKAGE_rtp2httpd=y/d' .config
-
-#删除自带和breakingbadboy自定义版本的dockerd、docker及依赖containerd、runc，使用自定义docker版本
-rm -rf feeds/fichenx/dockerd feeds/fichenx/docker
-rm -rf feeds/packages/utils/dockerd
-#git_sparse_clone main https://github.com/fichenx/openwrt-package dockerd && mv -n dockerd feeds/packages/utils/dockerd
-git_sparse_clone master https://github.com/coolsnowwolf/packages utils/dockerd && mv -n dockerd feeds/packages/utils/dockerd
-rm -rf feeds/packages/utils/docker
-#git_sparse_clone main https://github.com/fichenx/openwrt-package docker && mv -n docker feeds/packages/utils/docker
-git_sparse_clone master https://github.com/coolsnowwolf/packages utils/docker && mv -n docker feeds/packages/utils/docker
-rm -rf feeds/packages/utils/containerd
-git_sparse_clone master https://github.com/coolsnowwolf/packages utils/containerd && mv -n containerd feeds/packages/utils/containerd
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.2.0/g' feeds/packages/utils/containerd/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca/g' feeds/packages/utils/containerd/Makefile
-rm -rf feeds/packages/utils/runc
-git_sparse_clone master https://github.com/coolsnowwolf/packages utils/runc && mv -n runc feeds/packages/utils/runc
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.4.0/g' feeds/packages/utils/runc/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=94d566d8b017d6cdffc684560a4f069bb87f86534976c41d768711c85e194884/g' feeds/packages/utils/runc/Makefile
 
 # NaïveProxy
 rm -rf package/naiveproxy
