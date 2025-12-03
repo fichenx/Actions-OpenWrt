f1fd58720873d2c107116a4469522322ce96f969
尘事尘飞 <fichenx@gmail.com>
2025-12-03 15:47:49 +0800

    修复n2n编译问题
    给n2n的CMakeLists.txt增加补丁文件，修复语法顺序

---
+++ sh/ax6_imm_diy-part2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+echo "开始 自定义（fichen） 配置……"
+echo "========================="
+
+cp -rf $GITHUB_WORKSPACE/backup/001-fix-cmake-compatibility.patch feeds/packages/net/n2n/patches/
+
+echo "========================="
+echo " 自定义(fichen) 配置完成……"
