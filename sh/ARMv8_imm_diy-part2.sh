bf21df716b65da195650b3e3c87c8fe495f40aef
尘事尘飞 <fichenx@gmail.com>
2025-12-04 12:11:53 +0800

    修复containerd

--- sh/ARMv8_imm_diy-part2.sh
+++ sh/ARMv8_imm_diy-part2.sh
@@ -218,6 +218,7 @@ git_sparse_clone main https://github.com/fichenx/openwrt-package docker && mv -n
 #git_sparse_clone master https://github.com/coolsnowwolf/packages utils/containerd && mv -n containerd feeds/packages/utils/containerd
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.2.0/g' feeds/packages/utils/containerd/Makefile
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca/g' feeds/packages/utils/containerd/Makefile
+sed -i 's/containerd-shim,containerd-shim-runc-v1,//g' feeds/packages/utils/containerd/Makefile
 #rm -rf feeds/packages/utils/runc
 #git_sparse_clone master https://github.com/coolsnowwolf/packages utils/runc && mv -n runc feeds/packages/utils/runc
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.4.0/g' feeds/packages/utils/runc/Makefile
